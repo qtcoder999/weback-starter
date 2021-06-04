8f47b7415c372c86e27569477eb716eb91925924
Paras Anand <qtparas@gmail.com>
2021-06-04 22:28:57 +0530

    webpack initialized

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        group: './src/group-info/index.js',
+        message: './src/message-info/index.js',
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
