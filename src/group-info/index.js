b2e3a9cddef195c7da71b5138e59440e11a7ad45
Paras Anand <qtparas@gmail.com>
2021-06-05 19:58:00 +0530

    copy to clipboard package installed

--- src/group-info/index.js
+++ src/group-info/index.js
@@ -1,13 +1,12 @@
-const { gotoBottom, sleep, removeSelectors } = require("../utils/utils");
+const copy = require("copy-to-clipboard");
+const { gotoBottom, sleep } = require("../utils/utils");
 const { CSS_SELECTOR: { group, remove } } = require("./constants/constants");
 const { clickMoreButton, getTotalGroupMemberCount, grabInfo, getInfo } = require("./utils/utils");
 
 console.clear();
-console.log("Group info")
+console.log("WhatsApp Group Information")
 
 let totalGroupMemberCount = getTotalGroupMemberCount();
-console.log("totalGroupMemberCount", totalGroupMemberCount);
-console.log("getInfo().length", getInfo().length);
 
 try {
     clickMoreButton();
@@ -23,6 +22,8 @@ catch (e) { }
         await sleep(3000);
         console.log(getInfo().length);
     }
+
+    copy(JSON.stringify(getInfo()))
 
     console.log(getInfo());
 
